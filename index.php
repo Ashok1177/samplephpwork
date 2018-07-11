8af16640e25405cbb43656ef291a3352dcb5268d
Ashok1177 <41045669+Ashok1177@users.noreply.github.com>
2018-07-11 11:42:31 +0530

    code change

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <body>
 
-<h1>ashok the legend , he wanted tocrack aws job ,definetily i will</h1>
+<h1>ashok the legend , he wanted tocrack aws job ,definetily i will,need some patience</h1>
 
 <?php
 echo "version 1.0";
